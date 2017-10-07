df78636ad745f7073da48e50c584e582142b161b
Mike <mwwmail@gmail.com>
2017-10-07 20:33:56 +0000

    initial youtube loader

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import youtube from './loaders/youtube';
 
 class Root extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: 'https://www.youtube.com/watch?v=iYYRH4apXDo'
+    };
+
+    this.handleURLChange = this.handleURLChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleURLChange(e) {
+    this.setState({
+      url: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log('asdf');
+  }
+
   render() {
     return (
-      <h1>Radio</h1>
+      <div>
+        <h1>Radio</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="url">URL</label>
+          <input type="text" id="url"
+            onChange={this.handleURLChange}
+            value={this.state.url}
+          />
+          <input type="submit" value="view" />
+        </form>
+      </div>
     );
   }
 }
